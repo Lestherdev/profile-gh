c3c4bb3748c65f432df05bb338998bee26dffef1
Jordi Contreras <jordilestherdev@gmail.com>
2025-04-17 01:03:40 -0400

    Feat: banner capsule render

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@
 
 export default function Home() {
   return (
-  <>
-    <p>Hello World ! </p>
-  </>
+    <>
+      <div>
+        <img src="https://capsule-render.vercel.app/api?type=blur&color=gradient&customColorList=0,2,2,2,3&height=300&section=header&text=My%20Name%20is%20Jordi%20Lesther!🎮&desc=And%20i'm%20a%20Web%20Developer&fontSize=50&fontColor=1292b2&descSize=30&descAlignY=65&stroke=000000&animation=twinkling" />
+      </div>
+    </>
   );
 }
